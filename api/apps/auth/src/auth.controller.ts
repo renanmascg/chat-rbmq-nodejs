fe6e4637863c2b9fb87d5057e274c675a6104a1a
Renan Mascarenhas <renanmascg@gmail.com>
2023-09-18 17:06:04 -0300

    fix: Ajusta codigo para carregar automaticamente as tabelas do pg e ajustes de lint na aplicação.

--- api/apps/auth/src/auth.controller.ts
+++ api/apps/auth/src/auth.controller.ts
@@ -1,9 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
+import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
-  @MessagePattern({ cmd: 'get-user' })
+  constructor(private authService: AuthService) {}
+
+  @MessagePattern({ cmd: 'get-users' })
   async getUser(@Ctx() context: RmqContext) {
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
@@ -12,6 +15,18 @@ export class AuthController {
 
     channel.ack(originalMsg);
 
-    return { user: 'USER' };
+    return this.authService.getUsers();
+  }
+
+  @MessagePattern({ cmd: 'post-user' })
+  async postUser(@Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    console.log(channel);
+
+    channel.ack(originalMsg);
+
+    return this.authService.createUser();
   }
 }
--- api/apps/auth/src/auth.module.ts
+++ api/apps/auth/src/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
 
 @Module({
   imports: [
@@ -15,11 +16,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         url: configService.get('POSTGRES_URI'),
-        // autoLoadEntities: true,
-        // synchronize: true,
+        autoLoadEntities: true,
+        synchronize: true,
+        entities: [UserEntity],
       }),
       inject: [ConfigService],
     }),
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
